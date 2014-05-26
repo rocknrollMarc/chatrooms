34537dbf2bafa9b513a0b8e9d2f6c134393c0845
Marc Bluemner <Marc.Bluemner@mspag.com>
2014-05-26 18:17:13 +0200

    node chatroom server initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
+var cache = {};
+
+function send404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  responce.write('Error 404: resource not found.');
+  response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+  response.writeHead(
+    200,
+    {content-Type: mime.lookup(path.basename(filePath))}
+  );
+  response.end(fileContents);
+}
+
+function seveStatic(response, cache, absPath) {
+  if (cache[absPath])
+    sendFile(response, absPath, cache[absPath]);
+} else {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response);
+        } else {
+          cache[absPath] = data;
+        }
+      });
+    } else {
+      send404(response);
+    }
+  })
+}
